43bad3a11fa541685be652518cb48d5893008199
YannGUIOT <guiot_yann@hotmail.fr>
2023-07-17 15:33:08 +0200

    Velib API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+export const App = () => {
+
+
+  return (
+    <>
+      <h1>Carte des Velibs</h1>
+      <div id="mapid"></div>
+      <h1>Liste des Stations</h1>
+      <div id='stations'></div>
+    </>
+  )
+}
