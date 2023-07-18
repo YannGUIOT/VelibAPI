679c539a5b3d7dc1a7e0ba64576dad5ee7cb5feb
YannGUIOT <guiot_yann@hotmail.fr>
2023-07-18 11:42:50 +0200

    Vélib

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Stations } from './Components/Stations/Stations';
 import { useEffect, useState } from 'react';
 import { Map } from './Components/Map/Map';
+import './assets/font/Astrolab.ttf';
 import './App.css'
 
 
